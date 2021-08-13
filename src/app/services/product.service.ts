f6a96937475d5c8fc838fc5d03b0261e42451f53
ldex <laurent.duveau+git@gmail.com>
2021-08-13 15:57:19 -0400

    Insert Product

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -16,10 +16,16 @@ export class ProductService {
     this.initProducts();
   }
 
+  insertProduct(newProduct: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, newProduct);
+  }
+
   initProducts() {
+    let url:string = this.baseUrl + `?$orderby=ModifiedDate%20desc`;
+
     this.products$ = this
                         .http
-                        .get<Product[]>(this.baseUrl)
+                        .get<Product[]>(url)
                         .pipe(
                           delay(1500), // Fake delay!!!
                           tap(console.table),
